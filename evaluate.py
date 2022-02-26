a17736cac240a0e91af5c21f3bc7f474760fc90c
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-02-26 18:43:13 +0530

    refactor: move the code to run solver on all words in a function

--- evaluate.py
+++ evaluate.py
@@ -11,20 +11,30 @@ Evaluate Solver
 import json
 from pathlib import Path
 
+from tqdm import tqdm
+
 from vocab import Vocabulary
 from wordle import Wordle
 from solver import WordleSolver
-from tqdm import tqdm
 
 ###############################################################################
 
-STATUS = {}
+EVALUATION_FILE = Path("evaluation.json")
 
-for v in tqdm(Vocabulary().vocab):
-    s = WordleSolver(Wordle(word=v))
-    s.solve()
-    STATUS[v] = (s.wordle.solved, s.wordle.num_attempts)
+###############################################################################
 
-    Path("evaluation.json").write_text(json.dumps(STATUS, indent=2))
+
+def run_solver_on_all_words():
+    result = {'status': {}}
+
+    for v in tqdm(Vocabulary().vocab):
+        s = WordleSolver(Wordle(word=v))
+        s.solve()
+        result['status'][v] = (s.wordle.solved, s.wordle.num_attempts)
+
+        EVALUATION_FILE.write_text(json.dumps(result))
+
+    return result
+
 
 ###############################################################################
